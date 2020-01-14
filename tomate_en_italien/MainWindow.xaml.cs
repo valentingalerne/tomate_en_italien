6744938ae84981cc6581818e0d0599560b46da2c
Hippolyte THOMAS <hippolyte.thomas1@gmail.com>
2020-01-14 16:03:10 +0100

    Création d'un Timer

--- tomate_en_italien/MainWindow.xaml.cs
+++ tomate_en_italien/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace tomate_en_italien
 {
@@ -20,9 +21,31 @@ namespace tomate_en_italien
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static DateTime end = DateTime.Now.AddMinutes(25);
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //  DispatcherTimer setup
+            DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e) {
+
+            // Updating the Label which displays the timer
+            if (DateTime.Now.CompareTo(end) == -1) { 
+                var time = end.Subtract(DateTime.Now);
+                lblView.Content = String.Concat(time.Minutes, ":", time.Seconds);
+            } else {
+                lblView.Content = "C'est fini !";
+            }
+
+            // Forcing the CommandManager to raise the RequerySuggested event
+            CommandManager.InvalidateRequerySuggested();
         }
     }
 }
